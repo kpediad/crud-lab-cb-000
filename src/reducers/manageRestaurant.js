437c5ec2d0f3ce8be7bc00c4d723efeec22a8a22
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-28 23:39:33 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,35 +1,37 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
 export default function manageRestaurants(state = {
-  restaurants: [], reviews: []
+  restaurants: [],
+  reviews: [],
 }, action) {
-
   switch (action.type) {
+
     case 'ADD_RESTAURANT':
-      const restaurant = {
-        id: cuid(),
-        text: action.text
+
+      const restaurant = { text: action.text, id: cuidFn() };
+      return {
+        ...state,
+        restaurants: [ ...state.restaurants, restaurant]
       }
-      return {...state, restaurants: [...state.restaurants, restaurant]}
 
     case 'DELETE_RESTAURANT':
-      return {...state, restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)}
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return { ...state, restaurants}
 
     case 'ADD_REVIEW':
-      const review = {
-        id: cuid(),
-        restaurantId: action.review.restaurantId,
-        text: action.review.text
+
+      const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
+      return { ...state,
+        reviews: [...state.reviews, review]
       }
-      return {...state, reviews: [...state.reviews, review]}
 
     case 'DELETE_REVIEW':
-      return {...state, reviews: state.reviews.filter(reviews => review.id !== action.id)}
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return {...state, reviews }
 
     default:
       return state;
-  }
 
+  }
 };
